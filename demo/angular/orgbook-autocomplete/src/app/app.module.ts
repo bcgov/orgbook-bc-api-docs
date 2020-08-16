7b389a651ca075b7a0ec7463f7bacdc378918bca
Akiff Manji <akiff.manji@gmail.com>
2020-08-15 18:54:00 -0700

    Adds search module
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

--- demo/angular/orgbook-autocomplete/src/app/app.module.ts
+++ demo/angular/orgbook-autocomplete/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchModule } from './search/search.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SearchModule
   ],
   providers: [],
   bootstrap: [AppComponent]
