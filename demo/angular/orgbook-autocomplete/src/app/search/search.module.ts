2885492fadaf1ba1a52b12106bc7f911bde9f1d5
Akiff Manji <akiff.manji@gmail.com>
2020-08-15 18:55:12 -0700

    Adds search component
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

--- demo/angular/orgbook-autocomplete/src/app/search/search.module.ts
+++ demo/angular/orgbook-autocomplete/src/app/search/search.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SearchComponent } from './component/search/search.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SearchComponent],
   imports: [
     CommonModule
   ]
