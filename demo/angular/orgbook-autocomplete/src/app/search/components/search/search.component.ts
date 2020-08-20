8c569835bd52c43805aef063af7d4214510670c1
Akiff Manji <akiff.manji@gmail.com>
2020-08-19 23:37:34 -0700

    Debounced REST API calls to reduce server hits
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

--- demo/angular/orgbook-autocomplete/src/app/search/components/search/search.component.ts
+++ demo/angular/orgbook-autocomplete/src/app/search/components/search/search.component.ts
@@ -2,54 +2,65 @@ import { Component } from '@angular/core';
 
 import { SearchService } from '@app/search/services/search.service';
 
+import { Subject, of } from 'rxjs';
+import { switchMap, map, debounceTime, distinctUntilChanged } from 'rxjs/operators';
+
 import { AggregateAutocompleteResponse } from '@app/search/interfaces/aggregate-autocomplete-response';
 import { TopicResponse } from '@app/search/interfaces/topic-response';
-
+ 
 @Component({
   selector: 'ob-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent {
+  private autocompleteSearch$ = new Subject<string>();
+  private topicSearch$ = new Subject<string>();
+
   label = 'Registered BC Corporation Search';
   placeholder = 'Start typing to search the OrgBook database';
-  autocompleteResponse: AggregateAutocompleteResponse;
-  topicResponse: TopicResponse;
+
   search: any = {};
+
+  autocomplete$ = this.autocompleteSearch$
+    .pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap(q => {
+        if (!q) {
+          return of({ } as AggregateAutocompleteResponse);
+        }
+        return this.searchService.getAggregateAutocomplete(q);
+      })
+    );
+
+  topic$ = this.topicSearch$
+    .pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap(name => {
+        if (!name) {
+          return of({ } as TopicResponse);
+        }
+        return this.searchService.getTopic(name);
+      })
+    );
 
   constructor(private searchService: SearchService) { }
 
   onAutocomplete(q: string) {
-    if (!q) return;
-
     q = q.trim();
-    this.resetAutocomplete();
-
-    this.searchService.getAggregateAutocomplete(q)
-      .subscribe((res: AggregateAutocompleteResponse) => this.autocompleteResponse = res);
+    this.autocompleteSearch$.next(q);
   }
 
   onSearch(name: string) {
-    if (!name) return;
-
     name = name.trim();
-    this.resetTopic();
-
-    this.searchService.getTopic(name)
-      .subscribe((res: TopicResponse) => this.topicResponse = res);
+    this.topicSearch$.next(name);
   }
 
   onClearSearch() {
     this.search.value = '';
-    this.resetAutocomplete();
-    this.resetTopic();
-  }
-
-  private resetAutocomplete() {
-    this.autocompleteResponse = {} as AggregateAutocompleteResponse;
-  }
-
-  private resetTopic() {
-    this.topicResponse = {} as TopicResponse;
+    this.autocompleteSearch$.next('');
+    this.topicSearch$.next('');
   }
 }
