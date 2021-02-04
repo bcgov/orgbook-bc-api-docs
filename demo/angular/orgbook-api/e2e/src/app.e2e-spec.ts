04a2d86ef3623cb03874fd05d370d68df05dc596
Akiff Manji <akiff.manji@gmail.com>
2021-02-04 09:23:04 -0800

    Rename occurrences of 'autocomplete' to 'API'
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

--- demo/angular/orgbook-api/e2e/src/app.e2e-spec.ts
+++ demo/angular/orgbook-api/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('orgbook-autocomplete app is running!');
+    expect(page.getTitleText()).toEqual('orgbook-api app is running!');
   });
 
   afterEach(async () => {
--- demo/angular/orgbook-api/src/app/app.component.spec.ts
+++ demo/angular/orgbook-api/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'orgbook-autocomplete'`, () => {
+  it(`should have as title 'orgbook-api'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('orgbook-autocomplete');
+    expect(app.title).toEqual('orgbook-api');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('orgbook-autocomplete app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('orgbook-api app is running!');
   });
 });
