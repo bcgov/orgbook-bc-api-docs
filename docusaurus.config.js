9fbb78e23689610207552b560e973009c7546da1
Akiff Manji <amanji@petridish.dev>
2023-01-06 16:03:54 -0800

    Pass config parameters via env variables
    Signed-off-by: Akiff Manji <amanji@petridish.dev>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,23 +1,24 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
+require('dotenv').config()
 
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'OrgBook API',
-  tagline: 'OrgBook API Docs',
-  url: 'https://petridishdev.github.io',
-  baseUrl: '/orgbook-api',
+  title: `${process.env.PROJECT_TITLE}`,
+  tagline: `${process.env.PROJECT_TITLE} Docs`,
+  url: `https://${process.env.ORGANIZATION_NAME}.github.io`,
+  baseUrl: `/${process.env.PROJECT_NAME}`,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.png',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'petridisdev', // Usually your GitHub org/user name.
-  projectName: 'orgbook-api', // Usually your repo name.
+  organizationName: `${process.env.ORGANIZATION_NAME}`, // Usually your GitHub org/user name.
+  projectName: `${process.env.PROJECT_NAME}`, // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -36,7 +37,7 @@ const config = {
           routeBasePath: '/', // Serve the docs at the site's root
           sidebarPath: require.resolve('./sidebars.js'),
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/petridishdev/orgbook-api/tree/mkdocs',
+          editUrl: `https://github.com/${process.env.ORGANIZATION_NAME}/${process.env.PROJECT_NAME}/tree/${process.env.BRANCH_NAME}`,
         },
         blog: false, // Disable the blog plugin
         theme: {
@@ -71,8 +72,8 @@ const config = {
         },
         items: [
           {
-            href: 'https://github.com/petridishdev/orgbook-api',
             label: 'GitHub',
+            href: `https://github.com/${process.env.ORGANIZATION_NAME}/${process.env.PROJECT_NAME}`,
             position: 'right',
           },
         ],
@@ -94,7 +95,7 @@ const config = {
             items: [
               {
                 label: 'GitHub',
-                href: 'https://github.com/petridishdev/orgbook-api',
+                href: `https://github.com/${process.env.ORGANIZATION_NAME}/${process.env.PROJECT_NAME}`,
               },
             ],
           },
